dece819e14d3ed1bf92326edfd642d29d8b2f8d3
pasthortown <pastorchessmaster@gmail.com>
2019-01-29 18:10:19 -0500

    guard

--- mobileChat/src/app/app-routing.module.ts
+++ mobileChat/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
+import { AuthGuard } from './guard/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './layout/layout.module#LayoutModule' },
+  { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
